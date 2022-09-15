348af29f757cb262adf837baa1c445b855b104a4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-15 17:29:08 -0300

    Processamento de dados

---
+++ aula_2/primeiro_programa.py
@@ -0,0 +1,14 @@
+# imprimir strings no console
+print('Olá Mundo!')
+
+# imprimir strings com aspas duplas
+print("Olá Mundo!!")
+
+# operação aritmética
+print(2 + 3)
+
+# string textual
+print('2 + 3')
+
+# concatenação de textos
+print('2' + '3')
