f2fce0d8313e99696ed3243116a63d0eaaaa731a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-15 17:45:21 -0300

    Exemplificação de tipos de concatenação e operadores matemáticos

--- aula_2/primeiro_programa.py
+++ aula_2/primeiro_programa.py
@@ -12,3 +12,16 @@ print('2 + 3')
 
 # concatenação de textos
 print('2' + '3')
+
+# concatenando mensagens
+print('Olá, ' + 'mundo')
+print('Olá, ', 'Mundo!')
+
+# concatenando mensagens e números
+print('O resultado da soma de 2 + 3 é ', 2 + 3)
+
+# operadores matemáticos
+print(10 * (5 + 7) / 4)
+
+# operadores matemáticos (sem prioridade)
+print(10 * (5 + 7 / 4))
