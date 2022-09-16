1305c701079a7159a5a1c9c10bfb13c4ed0c70f4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-16 09:15:22 -0300

    Exemplos de atribuição de valor

---
+++ aula_2/variaveis_tipos_dados.py
@@ -0,0 +1,17 @@
+# atribuição de valores em variáveis
+nota = 8.5
+disciplina = 'Lógica de Programação e Algoritmos'
+
+print(nota)
+print(disciplina)
+
+print('Disciplina: ', disciplina, '. Nota: ', nota)
+
+a = 1
+b = 5
+
+resposta = a == b
+print(resposta)
+
+resposta = a != b
+print(resposta)
