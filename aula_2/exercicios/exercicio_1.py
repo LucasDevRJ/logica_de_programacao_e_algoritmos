09e3a6ea5391c21adc2fc4116b44a656e6839816
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-16 18:00:00 -0300

    Exercício 1 concluído

---
+++ aula_2/exercicios/exercicio_1.py
@@ -0,0 +1,12 @@
+somatorio = 1 + 2 + 3 + 4 + 5
+
+media = (23 + 19 + 31) / 3
+
+numeroVezes = 403 // 73
+
+sobra = 403 % 73
+
+print('Somatorio = {}' .format(somatorio))
+print('Média = {}' .format(media))
+print('Número de vezes = {}' .format(numeroVezes))
+print('Sobra = {}' .format(sobra))
