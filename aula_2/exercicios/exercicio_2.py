658b10f08aa75e8038a08fe3f334db847ec5f9c3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-16 18:13:07 -0300

    Excercício 2 concluído

---
+++ aula_2/exercicios/exercicio_2.py
@@ -0,0 +1,9 @@
+elevacao = 2**10
+
+diferenca = abs(54 - 57)
+
+menorValor = min(34, 29, 31)
+
+print('2 elevado a decima potência: {}'.format(elevacao))
+print('A diferença entre 54 e 57 = {}' .format(diferenca))
+print('O menor valor = {}' .format(menorValor))
