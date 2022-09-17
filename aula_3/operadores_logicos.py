db64e6406f0b24d6e3a4d35bfa0263803ffff564
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-17 17:52:02 -0300

    Criação de exemplo de operadores lógicos

---
+++ aula_3/operadores_logicos.py
@@ -0,0 +1,22 @@
+# negação
+x = True
+y = False
+
+print(not x)
+print(not y)
+
+# adição
+print(x and y)
+
+# alternativa
+print(x or y)
+
+x = 10
+y = 1
+
+res = not x > y
+print(res)
+
+z = 5.5
+res = x > y and z == y
+print(res)
