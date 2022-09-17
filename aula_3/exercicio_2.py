97187be1c6383f923962ed90552d8123c642a7b8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-17 17:18:14 -0300

    Exercício de condicional composta concluída

---
+++ aula_3/exercicio_2.py
@@ -0,0 +1,6 @@
+valor = int(input('Digite um valor inteiro: '))
+
+if (valor % 2 == 0) :
+    print('O valor {} é um número par.' .format(valor))
+else :
+    print('O valor {} é um número impar.' .format(valor))
