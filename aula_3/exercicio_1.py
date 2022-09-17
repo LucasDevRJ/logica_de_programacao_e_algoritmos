e1ed3e80f86747632ad397673e5696308d234395
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-17 17:06:20 -0300

    Exercício condicional simples completado

---
+++ aula_3/exercicio_1.py
@@ -0,0 +1,5 @@
+valor1 = int(input('Digite um valor numérico inteiro: '))
+valor2 = int(input('Digite o segundo valor numérico inteiro: '))
+
+if (valor1 > valor2) :
+    print('O primeiro valor digitado é maior que o segundo!')
