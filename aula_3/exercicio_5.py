1b007b226f41eab8f0fda121d44702439e03cea1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-17 18:46:14 -0300

    Exercício 5 concluído com sucesso

---
+++ aula_3/exercicio_5.py
@@ -0,0 +1,21 @@
+print('----------|Cardápio|----------')
+print('1 - Maça')
+print('2 - Laranja')
+print('3 - Banana')
+print('------------------------------')
+
+opcao = int(input('Digite a opção desejada: '))
+
+if (opcao == 1 or opcao == 2 or opcao == 3) :
+    unidades = int(input('Digite a quantidade desejada: '))
+    if (opcao == 1) :
+        precoFruta = 2.30
+    elif (opcao == 2) :
+        precoFruta = 3.60
+    elif (opcao == 3) :
+        precoFruta = 1.85
+else :
+    print("Opção inválida")
+
+precoTotal = precoFruta * unidades
+print('Preço total: R$ {}' .format(precoTotal))
