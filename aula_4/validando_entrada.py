990992da39438bf53de57bd29eb4a9b227be61e4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-19 22:10:22 -0300

    Exemplo de programa com validação de dados.

---
+++ aula_4/validando_entrada.py
@@ -0,0 +1,5 @@
+x = int(input('Digite um valor maior que 0: '))
+
+while (x <= 0) :
+    x = int(input('Digite um valor maior que 0: '))
+    print('Você digitou {}. Encerrando o programa...' .format(x))
