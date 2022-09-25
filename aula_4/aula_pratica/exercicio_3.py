51f3aacb204cb252ba01b4b2d597353b1d4b97cf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-25 10:59:16 -0300

    Exercício 3 concluído.

---
+++ aula_4/aula_pratica/exercicio_3.py
@@ -0,0 +1,32 @@
+valor = int(input('Digite um valor monetário: R$ '))
+
+while (valor > 0) :
+    if (valor >= 100):
+        numeroCedulas = valor // 100
+        print('Quantidade de cédulas de R$ 100,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 100 * numeroCedulas
+
+    if (valor >= 50):
+        numeroCedulas = valor // 50
+        print('Quantidade de cédulas de R$ 50,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 50 * numeroCedulas
+
+    if (valor >= 20):
+        numeroCedulas = valor // 20
+        print('Quantidade de cédulas de R$ 20,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 20 * numeroCedulas
+
+    if (valor >= 10):
+        numeroCedulas = valor // 10
+        print('Quantidade de cédulas de R$ 10,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 10 * numeroCedulas
+
+    if (valor >= 5):
+        numeroCedulas = valor // 5
+        print('Quantidade de cédulas de R$ 5,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 5 * numeroCedulas
+
+    if (valor >= 1):
+        numeroCedulas = valor // 1
+        print('Quantidade de cédulas de R$ 1,00 necessárias para pagar o valor: %.0f' % numeroCedulas)
+        valor -= 1 * numeroCedulas
