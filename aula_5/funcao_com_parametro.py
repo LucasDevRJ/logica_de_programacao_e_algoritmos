c46797506095eb5ae4418e2aed459554003b22f9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-26 18:48:42 -0300

    Exemplo de função com parâmetro.

---
+++ aula_5/funcao_com_parametro.py
@@ -0,0 +1,15 @@
+def realce(s1) :
+    print('|', '—' * 10, '|')
+    print('|', '—' * 10, '|')
+    print(s1)
+    print('|', '—' * 10, '|')
+    print('|', '—' * 10, '|')
+
+realce('    Lucas    ')
+
+def sub2(x, y) :
+    res = x - y
+    print(res)
+
+sub2(5, 7)
+sub2(7, 5)
