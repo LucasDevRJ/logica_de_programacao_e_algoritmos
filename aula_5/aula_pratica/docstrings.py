f6361ff30f5a71f90f662e28135145d1d0206b19
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-28 07:24:57 -0300

    Exemplo de docstring.

---
+++ aula_5/aula_pratica/docstrings.py
@@ -0,0 +1,13 @@
+def soma(x = 0, y = 0, z = 0) :
+    #para criar a documentação da função
+    """
+    Função que soma até três valores inteiros.
+    :param x: valor inteiro opcional
+    :param y: valor inteiro opcional
+    :param z: valor inteiro opcional
+    :return: soma dos valores
+    """
+    return x + y + z
+
+print(soma(2, 3))
+help(soma)
