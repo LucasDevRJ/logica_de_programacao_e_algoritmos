4e779e45f8537edbc9e2ada742508cbd558ef64f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-01 20:03:20 -0300

    Exemplo de validação de dados.

---
+++ aula_6/validando_dados.py
@@ -0,0 +1,8 @@
+s1 = 'Lógica de Programação e Algoritmos'
+s2 = '42'
+
+print(s1.isalnum()) #retorna se tem número ou não
+print(s2.isalnum())
+
+print(s1.isalpha() #somente o alfábeto
+print(s2.isalpha())
