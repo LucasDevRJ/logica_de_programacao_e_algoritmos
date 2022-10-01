73c7ffa12c11d045f5301a5dffcaeeaf89226638
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-01 18:37:53 -0300

    Lista com dicionario.

---
+++ aula_6/listas_com_dicionarios.py
@@ -0,0 +1,16 @@
+games = []
+game1 = {'nome':'Super Mario',
+        'videogame':'Super Nintendo',
+        'ano':1990}
+
+game2 = {'nome':'Zelda Ocarina of Time',
+        'desenvolvedora':'Nintendo 64',
+        'ano':1998}
+
+game3 = {'nome':'Pokemon Yellow',
+        'desenvolvedora':'Game Boy',
+        'ano':1999}
+
+games = [game1, game2, game3]
+
+print(games)
